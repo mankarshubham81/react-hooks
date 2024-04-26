73323d8dd61d44cca66eeaa7675d1d9bfd7f9c0a
Shubham Mankar <mankarshubham81@gmail.com>
2024-04-26 13:47:42 +0530

    dwawback of useRef hook

--- src/AllHooks/HookUseRef.jsx
+++ src/AllHooks/HookUseRef.jsx
@@ -5,15 +5,20 @@ const HookUseRef = () => {
         num: 1,
         id: "abcd", 
     });
+    const [name, setName] = useState("")
 
     // const [rendered, setRendered] = useState(1);
     const renderRef = useRef(1)
+    const countRef = useRef()
     // useEffect(() => {
     //   setRendered(prev => prev + 1);
     // })
+    // useEffect(() => {
+    //   renderRef.current = renderRef.current + 1;
+    // })
     useEffect(() => {
-      renderRef.current = renderRef.current + 1;
-    })
+      countRef.current = name;
+    }, [name])
 
 
     
@@ -44,10 +49,16 @@ const HookUseRef = () => {
     return (
       <div >
         <button  onClick={handleSubstract}>-</button>
+        {/* <p ref={countRef} >{count.num}</p> */}
         <p>{count.num}</p>
         <p>{count.id}</p>
         <button  onClick={handleAdd}>+</button>
         <p>Renderd : {renderRef.current} times</p>
+        <input ref={countRef} onChange={(e) => setName(e.target.value)} />
+        {/* <button onClick={() => {
+          countRef.current.innerHTML = 10;
+        }}>Change to 10</button> */}
+        <p>{name} is state and {countRef.current} is ref</p>
       </div>
     )
 }
