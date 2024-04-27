47dbd73fe37e40efbe8a76c04fb695d97efce6b4
Shubham Mankar <mankarshubham81@gmail.com>
2024-04-27 17:13:07 +0530

    added code changes for custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,16 @@ import HookUseContext from './AllHooks/HookUseContext'
 import HookUseReducer from './AllHooks/HookUseReducer'
 import HookUseImperativeHandle from './AllHooks/HookUseImperativeHandle'
 import HookUseLayoutEffect from './AllHooks/HookUseLayoutEffect'
+import useFetchAPI from './AllHooks/useFetchAPI'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  const {data, loading, error} = useFetchAPI("https://jsonplaceholder.typicode.com/users");
+
+  if(error) return <p>Error....!</p>
+  if(loading) return <p>Loading....!</p>
+  console.log(data)
   return (
     <>
       {/* <HookUseState/> */}
@@ -26,7 +32,13 @@ function App() {
       {/* <HookUseContext/> */}
       {/* <HookUseReducer/> */}
       {/* <HookUseImperativeHandle/> */}
-      <HookUseLayoutEffect />
+      {/* <HookUseLayoutEffect /> */}
+      <ul>
+        {data && data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+
+      </ul>
     </>
   )
 }
